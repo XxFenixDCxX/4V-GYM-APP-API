25d08934ef9d2e94b9727d3ab618821ee112a242
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2023-12-29 12:34:27 +0100

    Hecho parte de post

--- src/Controller/MonitorController.php
+++ src/Controller/MonitorController.php
@@ -4,20 +4,75 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Monitor;
 
 class MonitorController extends AbstractController
 {
-    #[Route('/monitor', name: 'get_monitors', methods: ['GET'])]
+    #[Route('/monitors', name: 'get_monitors', methods: ['GET'])]
     public function getAll(EntityManagerInterface $entityManager): JsonResponse
     {
         $monitors = $entityManager->getRepository(Monitor::class)->findAll();
 
-        // Convertir la colección a un array
-        $monitorsArray = array_map(fn($monitor) => $monitor->toArray(), $monitors);
+        $monitorsArray = array_map(fn ($monitor) => $monitor->toArray(), $monitors);
 
         return $this->json($monitorsArray);
+    }
+
+    #[Route('/monitors', name: 'create_monitor', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $monitor = new Monitor();
+        $monitor->setName($data['name']);
+        $monitor->setEmail($data['email']);
+        $monitor->setPhone($data['phone']);
+        $monitor->setPhoto($data['photo']);
+
+        $entityManager->persist($monitor);
+        $entityManager->flush();
+
+        return $this->getAll($entityManager);
+    }
+
+    #[Route('/monitors/{id}', name: 'update_monitor', methods: ['PUT'])]
+    public function update(Request $request, EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $monitor = $entityManager->getRepository(Monitor::class)->find($id);
+
+        if (!$monitor) {
+            throw $this->createNotFoundException('Monitor not found');
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $monitor->setName($data['name']);
+        $monitor->setEmail($data['email']);
+        $monitor->setPhone($data['phone']);
+        $monitor->setPhoto($data['photo']);
+
+
+
+        $entityManager->flush();
+
+        return $this->json($monitor->toArray());
+    }
+
+    #[Route('/monitors/{id}', name: 'delete_monitor', methods: ['DELETE'])]
+    public function delete(EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $monitor = $entityManager->getRepository(Monitor::class)->find($id);
+
+        if (!$monitor) {
+            throw $this->createNotFoundException('Monitor not found');
+        }
+
+        $entityManager->remove($monitor);
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Monitor deleted']);
     }
 }
